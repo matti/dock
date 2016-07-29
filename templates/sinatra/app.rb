3361ac768fdff8ad6168e93ceaa1cccd4161258f
Matti Paksula <matti.paksula@iki.fi>
2016-07-29 16:53:25 +0300

    wip

--- templates/sinatra/app.rb
+++ templates/sinatra/app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
+if development?
+  require 'sinatra/reloader'
+  require 'byebug'
+end
+
 get '/' do
-  'rock on!'
+  'rock on! Also try <a href="/byebug">byebug</a>!'
+end
+
+get '/byebug' do
+  byebug
+
+  "great!"
 end
